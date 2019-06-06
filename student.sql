1d6a0d8ed785a8e2f3a340864a64e92c1103c85d
wgx <wgx@163.com>
2019-06-06 18:45:14 +0800

    添加院系表。为学生表添加外键

--- student.sql
+++ student.sql
@@ -1,5 +1,12 @@
-create table student(
-Sno int primary key auto_increment,
-Sname char(10),
-Sex char(2)
+CREATE TABLE department(
+dno INT PRIMARY KEY auto_increment,
+dname CHAR(20)
 )
+
+create table student(
+	Sno int primary key auto_increment,
+	Sname char(10),
+	Sex char(2),
+	dno INT FOREIGN KEY reference department(dno)
+)
+
